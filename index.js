b48a16f6f503e04882ca382592ac74bafca80049
Isabel de Waal <isabel@digitalreachopm.com>
2026-06-19 14:06:05 +0200

    Sixth commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -42,7 +42,30 @@ app.get('/update',async (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update', async (req, res) => {
+    const update = {
+        properties : {
+            "name": req.body.name,
+            "author": req.body.author,
+            "genre": req.body.genre
+    }
+
+    }
+    const updateBooks = 'https://api.hubspot.com/crm/v3/objects/2-64393620';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try{
+    const request = await axios.post(updateBooks, update, { headers })
+    res.redirect('/')
+    } catch (error) {
+        console.error(error);
+    }
+
+
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
