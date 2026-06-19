fa855f23a22296c80d76c24cd26c24bb563fdaca
Isabel de Waal <isabel@digitalreachopm.com>
2026-06-19 12:08:42 +0200

    Fith commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
 app.get('/', async (req, res) => {
     const books = 'https://api.hubspot.com/crm/v3/objects/2-64393620/?properties=name,author,genre';
     const headers = {
@@ -27,8 +26,7 @@ app.get('/', async (req, res) => {
     try{
     const response = await axios.get(books, { headers })
     const data = response.data.results
-    console.log(JSON.stringify(data, null, 2)); // ADD THIS LINE
-    res.render('books', {title: 'Books | Hubspot APIs', data})
+    res.render('view_books', {title: 'Books in Hubspot', data})
     } catch (error) {
         console.error(error);
     }
@@ -36,7 +34,11 @@ app.get('/', async (req, res) => {
 })
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update',async (req, res) => {
+    res.render('updates', {title: 'Add a Book to HubSpot'})
+ 
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
